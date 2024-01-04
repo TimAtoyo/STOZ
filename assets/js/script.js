3da21fb441b6bbae888049caec957b5d296ba3eb
Stim Fry Mac <timothy.atoyo@gmail.com>
2024-01-04 18:42:19 +0000

    Tests the results from the api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,43 @@
+// 
+var countryCodEl = document.querySelector('.country-option');
+
 // Testing api to see the results
 
 var url = `https://www.travel-advisory.info/api`;
 
 fetch(url)
-.then( response => response.json())
-.then(data => log(data))
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.data);
+    
+    // Results object
+    var results = data.data;
+
+    // Loop to create options and append to html
+
+    for (const key in results) {
+        if (Object.hasOwnProperty.call(results, key)) {
+            
+            const value = results[key];
+            
+            for (const key in value) {
+                if (Object.hasOwnProperty.call(value, key)) {
+                    const countryName = value.name;
+                    // iso value
+                    var countryCode = value.so_alpha2;
+                    console.log(countryName);
+
+
+                    // Creatong option element
+                    var optionEl = document.createElement('option');
+                    optionEl.textContent = countryName;
+                    optionEl.setAttribute('value', countryCode);
+                    // Append the options to the element on html Document
+                    countryCodEl.append(optionEl)
+
+                    
+                }
+            }
+        }
+    }
+  });
